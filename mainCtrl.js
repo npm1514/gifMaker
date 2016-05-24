efe5ef1ed13a9fab3237b8ced35145789f21b640
Nick Marucci <npm1514@gmail.com>
2016-05-23 22:49:51 -0600

    adding gif maker

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,3 +1,11 @@
-angular.module("app").controller("mainCtrl", function($scope, mainService) {
+angular.module("gifSpot").controller("mainCtrl", function($scope, mainService) {
+  $scope.gifs = [];
+  $scope.findGif = function(key) {
+    mainService.findGif(key)
+    .then(function(response) {
+      $scope.gifs = response.data;
+      console.log($scope.gifs);
+    });
+  };
 
 });
--- mainService.js
+++ mainService.js
@@ -1,3 +1,12 @@
-angular.module("app").service("mainService", function() {
+angular.module("gifSpot").service("mainService", function($http) {
+
+  this.findGif = function(key) {
+      return $http({
+        method: 'GET',
+        url: "http:api.giphy.com/v1/gifs/search?q=" + key + "&api_key=dc6zaTOxFJmzC"
+      }).then(function(response) {
+          return response.data;
+      });
+    };
 
 });
